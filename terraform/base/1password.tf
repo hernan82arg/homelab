3013f917d80f5628a55436bfb834d187aabb06b9
Hernan Dominguez <hernan.dc@gmail.com>
2026-03-22 22:11:15 +0100

    fix: 1p secrets

--- terraform/base/1password.tf
+++ terraform/base/1password.tf
@@ -14,24 +14,18 @@ resource "kubernetes_manifest" "op" {
       "namespace" = kubernetes_namespace.onepassword.metadata[0].name
     }
     "stringData" = {
-      "1password-credentials.json" = base64encode(var.onepassword_credentials)
+      "1password-credentials.json" = var.onepassword_credentials
     }
     "type" = "Opaque"
   }
 }
 
-resource "kubernetes_manifest" "op_token" {
-  computed_fields = ["stringData"]
-  manifest = {
-    "apiVersion" = "v1"
-    "kind"       = "Secret"
-    "metadata" = {
-      "name"      = "onepassword-token"
-      "namespace" = kubernetes_namespace.onepassword.metadata[0].name
-    }
-    "stringData" = {
-      "token" = base64encode(var.onepassword_token)
-    }
-    "type" = "Opaque"
+resource "kubernetes_secret" "op_token" {
+  metadata {
+    name      = "onepassword-token"
+    namespace = kubernetes_namespace.onepassword.metadata[0].name
+  }
+  data = {
+    "token" = var.onepassword_token
   }
 }
